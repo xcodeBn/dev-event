a30f8938e8577b8d0a44c023e81ba14f20a52c08
XcodeBn <86073708+xcodeBn@users.noreply.github.com>
2026-01-18 13:13:08 +0200

    add PostHog integration for event tracking and update styles
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased min-h-screen`}
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased min-h-screen dark`}
       >
 
       <NavBar></NavBar>
--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -2,12 +2,18 @@
 
 import React from 'react'
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const ExploreBtn = () => {
+    const handleClick = () => {
+        console.log("btn clicked");
+        posthog.capture("explore_events_clicked", {
+            button_location: "hero_section",
+        });
+    };
+
     return (
-        <button onClick={
-            ()=>{console.log("btn clicked")
-        }} type={"button"} id={"explore-btn"}  className={"mt-7 mx-auto"}>
+        <button onClick={handleClick} type={"button"} id={"explore-btn"}  className={"mt-7 mx-auto"}>
             <a href={"#events"}>Explore Events
             <Image src={"/icons/arrow-down.svg"} alt={"arrow down icon"} width={24} height={24}/></a>
         </button>
